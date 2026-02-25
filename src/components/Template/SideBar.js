98590f2bc31b66383c8fad8dbfe9c1c213900470
Shikhar Bhatt <shikhar.bhatt011@gmail.com>
2026-02-25 23:21:51 +0530

    feat: add dark mode

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import ContactIcons from '../Contact/ContactIcons';
+import ThemeToggle from './ThemeToggle';
 
 const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
 
@@ -32,6 +33,7 @@ const SideBar = () => (
     </section>
 
     <section id="footer">
+      <ThemeToggle />
       <ContactIcons />
       <p className="copyright">&copy; Shikhar Bhatt <Link to="/">shikharbhatt.github.io</Link>.</p>
     </section>
